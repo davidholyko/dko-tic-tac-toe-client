6ce5bf0ed792c529236b84bf6f6cade96edd873d
davidholyko <david.holy.ko@gmail.com>
2019-02-18 11:59:12 -0500

    improve logic-test algorhythm

--- assets/scripts/logic/logic-test.js
+++ assets/scripts/logic/logic-test.js
@@ -4,26 +4,27 @@ const checkForWinner = (size, board = []) => {
   if (!board.length) { return }
   const winner = []
   const boardSize = Math.sqrt(board.length) | 0
+  const lines = { boardSize, boardStart: 0, boardIncrement: 1, diagonal: boardSize - 1 }
 
-  const checkLine = function (initialValue, difference, jump = 0, cycle = 1) {
+  const checkLine = function (axis = 3, initialValue, difference, jump = 0, cycle = 1) {
     const check = []; cycle = cycle - 1
     for (let i = 0; i < boardSize; i++) { check.push(board[initialValue + difference * i]) }
-    $('#demo-output').append(`<h1>checking indexes: ${check}</h1>`)
+    if (axis <= 3) { $('#demo-output').append(`<h1>cycle: ${cycle} axis: ${axis} checking indexes: ${check}</h1>`) }
     // console.log(`checking values: ${check}`) // <----- uncomment to check which values are checked
     if (check.every(letter => letter === check[0])) { winner.push(check[0]) }
-    if (initialValue === difference) { return checkLine(initialValue - difference, difference + 2) }
-    if (!cycle) { return }
-    return checkLine(initialValue + jump, difference, jump, cycle)
+    if (initialValue === difference) { return checkLine(axis, initialValue - difference, difference + 2, 0, 1) }
+    if (!cycle) {
+      axis = axis - 1
+      if (!axis && !cycle) { return }
+      if (axis === 3) { return checkLine(axis, lines.boardStart, lines.boardIncrement, lines.boardSize, lines.boardSize) }
+      if (axis === 2) { return checkLine(axis, lines.boardStart, lines.boardSize, lines.boardIncrement, lines.boardSize) }
+      if (axis === 1) { return checkLine(axis, lines.diagonal, lines.diagonal, 0, 1) }
+    }
+    return checkLine(axis, initialValue + jump, difference, jump, cycle)
   }
 
-  const checkLines = board => {
-    const lines = { boardSize, boardStart: 0, boardIncrement: 1, diagonal: boardSize - 1 }
-    checkLine(lines.boardStart, lines.boardIncrement, lines.boardSize, lines.boardSize)
-    checkLine(lines.boardStart, lines.boardSize, lines.boardIncrement, lines.boardSize)
-    checkLine(lines.diagonal, lines.diagonal)
-  }
+  checkLine(4)
 
-  checkLines(board)
   return winner
 }
 
